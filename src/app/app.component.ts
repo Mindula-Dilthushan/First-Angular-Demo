afcdef437f97e1d60b7752d2c5d5d4081c206b7e
Mindula-Dilthushan <66836116+Mindula-Dilthushan@users.noreply.github.com>
2021-03-26 09:10:57 +0530

    title name change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'First-Angular';
+  title = 'Alpha X Software Company';
 }
